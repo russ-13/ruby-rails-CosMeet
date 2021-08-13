891f998c9bbd2ef1d545c4056db298c53eee555f
russ13 <contactrussellmoore@gmail.com>
2021-08-13 12:55:10 +1000

    fixed schema.rb duplication error

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_13_022756) do
+ActiveRecord::Schema.define(version: 2021_08_13_023721) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2021_08_13_022756) do
   end
 
   create_table "active_storage_blobs", force: :cascade do |t|
+    t.integer "ID"
     t.string "key"
     t.string "checksum"
     t.string "service_name"
