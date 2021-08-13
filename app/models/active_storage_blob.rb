f41265cf4af8ef80413ec250122dbb8110b2df13
russ13 <contactrussellmoore@gmail.com>
2021-08-13 16:06:13 +1000

    everything is broken starting again

--- app/models/active_storage_blob.rb
+++
@@ -1,2 +0,0 @@
-class ActiveStorageBlob < ApplicationRecord
-end
--- test/models/active_storage_attachment_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ActiveStorageAttachmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
